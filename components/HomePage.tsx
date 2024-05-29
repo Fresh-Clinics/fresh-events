a2c25945f7a3dc2b0b2abf8c1294ea49c6862a76
Fresh-Clinics <community@freshclinics.com>
2024-05-29 22:52:42 +1000

    Update HomePage.tsx

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -8,9 +8,9 @@ export function HomePage() {
           <div className="container px-4 md:px-6">
             <div className="grid gap-10">
               <div className="space-y-4">
-                <h2 className="text-3xl font-bold sm:text-xl md:text-3xl">
+                <h1 className="text-3xl font-bold sm:text-xl md:text-3xl">
                   Events
-                </h2>
+                </h1>
                 <EventList />
               </div>
             </div>
