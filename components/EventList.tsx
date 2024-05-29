a2f8301859fbf7e6550efc16147c0b6334643b46
Fresh-Clinics <community@freshclinics.com>
2024-05-29 23:15:58 +1000

    Update EventList.tsx

--- components/EventList.tsx
+++ components/EventList.tsx
@@ -61,12 +61,12 @@ const EventList: React.FC = () => {
       {futureEvents.map(({ event, api_id }) => (
         <a className="event-box flex items-start justify-between" key={api_id} href={event.url} target="_blank" rel="noopener noreferrer">
           <div className="flex-grow">
-            <p className="text-md font-semibold text-gray-500">
+            <p className="text-md text-gray-500 dark:text-gray-400">
               {moment(event.start_at).tz("Australia/Sydney").format('DD MMM')}
               <span style={{ opacity: 0.5 }}> {moment(event.start_at).tz("Australia/Sydney").format('dddd')}</span>
             </p>
             <div className="grid gap-1">
-              <h2 className="text-lg font-semibold">{event.name}</h2>
+              <h3 className="text-lg font-semibold">{event.name}</h3>
               <p className="text-sm text-gray-500 dark:text-gray-400">
                 {moment(event.start_at).tz("Australia/Sydney").format('h:mm A')} - {moment(event.end_at).tz("Australia/Sydney").format('h:mm A z')}
               </p>
