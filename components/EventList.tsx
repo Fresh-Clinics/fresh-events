1e329f14ce21bb71b9fe484b500392400ab0b008
Fresh-Clinics <community@freshclinics.com>
2024-05-29 23:09:12 +1000

    Update EventList.tsx

--- components/EventList.tsx
+++ components/EventList.tsx
@@ -59,27 +59,27 @@ const EventList: React.FC = () => {
   return (
     <div className="grid gap-4">
       {futureEvents.map(({ event, api_id }) => (
-        <a className="event-box flex items-start justify-between" key={api_id} href={event.url} target="_blank" rel="noopener noreferrer">
-          <div className="flex-grow">
-            <p className="text-md text-gray-500 dark:text-gray-400">
+        <a className="event-box flex flex-col justify-between relative" key={api_id} href={event.url} target="_blank" rel="noopener noreferrer">
+          <div>
+            <p className="text-md font-semibold text-gray-500 dark:text-gray-400">
               {moment(event.start_at).tz("Australia/Sydney").format('DD MMM')}
               <span style={{ opacity: 0.5 }}> {moment(event.start_at).tz("Australia/Sydney").format('dddd')}</span>
             </p>
             <div className="grid gap-1">
-              <h2 className="text-lg font-semibold dark-green">{event.name}</h2>
+              <h2 className="text-lg font-semibold">{event.name}</h2>
               <p className="text-sm text-gray-500 dark:text-gray-400">
                 {moment(event.start_at).tz("Australia/Sydney").format('h:mm A')} - {moment(event.end_at).tz("Australia/Sydney").format('h:mm A z')}
               </p>
             </div>
-            <p className="text-md text-green-500 font-semibold mt-auto">
-              Register To Attend
-            </p>
           </div>
+          <p className="text-md text-green-500 font-semibold">
+            Register To Attend
+          </p>
           {event.cover_url && (
             <img
               src={event.cover_url}
               alt={event.name}
-              className="rounded-md max-w-[20%] ml-4"
+              className="absolute top-0 right-0 rounded-md max-w-[20%] ml-4 p-4"
               style={{ borderRadius: '4px', padding: '15px' }}
             />
           )}
