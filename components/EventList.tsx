859a86da5cb920b249962ed9b0f08a74f813a232
Fresh-Clinics <community@freshclinics.com>
2024-05-29 18:15:26 +1000

    Update EventList.tsx

--- components/EventList.tsx
+++ components/EventList.tsx
@@ -56,7 +56,7 @@ export const EventList = async () => {
               minute: "2-digit",
             })}
           </p>
-        <img src="{event.cover_url}"></img>
+        {event.cover_url}
           <p className="text-md text-green-500 font-semibold padding-top">
               Register To Attend
           </p>
