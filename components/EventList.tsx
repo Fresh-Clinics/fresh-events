09f2dbdc9c1330bbba6d7a8a4407fe54b6bb2ace
Fresh-Clinics <community@freshclinics.com>
2024-05-29 23:05:21 +1000

    Update EventList.tsx

--- components/EventList.tsx
+++ components/EventList.tsx
@@ -60,7 +60,7 @@ const EventList: React.FC = () => {
     <div className="grid gap-4">
       {futureEvents.map(({ event, api_id }) => (
         <a className="event-box flex items-start justify-between" key={api_id} href={event.url} target="_blank" rel="noopener noreferrer">
-          <div>
+          <div className="flex-grow">
             <p className="text-md text-gray-500 dark:text-gray-400">
               {moment(event.start_at).tz("Australia/Sydney").format('DD MMM')}
               <span style={{ opacity: 0.5 }}> {moment(event.start_at).tz("Australia/Sydney").format('dddd')}</span>
@@ -70,17 +70,17 @@ const EventList: React.FC = () => {
               <p className="text-sm text-gray-500 dark:text-gray-400">
                 {moment(event.start_at).tz("Australia/Sydney").format('h:mm A')} - {moment(event.end_at).tz("Australia/Sydney").format('h:mm A z')}
               </p>
-              <p className="text-md text-green-500 font-semibold padding-top">
-                Register To Attend
-              </p>
             </div>
+            <p className="text-md text-green-500 font-semibold mt-auto">
+              Register To Attend
+            </p>
           </div>
           {event.cover_url && (
             <img
               src={event.cover_url}
               alt={event.name}
-              className="rounded-md max-w-[20%] ml-4 img-format"
-              style={{ borderRadius: '4px' }}
+              className="rounded-md max-w-[20%] ml-4"
+              style={{ borderRadius: '4px', padding: '15px' }}
             />
           )}
         </a>
