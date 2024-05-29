11acf71970321889553ca71d2c5c4a6a55f0a18e
Fresh-Clinics <community@freshclinics.com>
2024-05-29 19:04:55 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import { HomePage } from "@/components/HomePage";
+import Image from "next/image";
 
 export default function Home() {
-  return <HomePage />;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+       <iframe
+  src="https://lu.ma/embed/calendar/cal-PfKCHyYHQHvdr87/events"
+  width="80%"
+  height="100%"
+  frameborder="0"
+  style="border:none!important;"
+></iframe>
+      </div>
+    </main>
+  );
 }
