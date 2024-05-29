3682bf2956297a0940dc8ffd4981dd8af23fd19c
Fresh-Clinics <community@freshclinics.com>
2024-05-29 17:29:15 +1000

    Update HomePage.tsx

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -2,7 +2,7 @@ import { EventList } from "@/components/EventList";
 
 export function HomePage() {
   return (
-    <div className="flex min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div className="flex min-h-screen bg-gray-100">
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32">
           <div className="container px-4 md:px-6">
