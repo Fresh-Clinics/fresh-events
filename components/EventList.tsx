e8e79fc3a0ae0abfd6aa123624ea63a475eac933
Fresh-Clinics <community@freshclinics.com>
2024-05-29 21:05:43 +1000

    Update EventList.tsx

--- components/EventList.tsx
+++ components/EventList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import moment from 'moment-timezone';
 
 type EventData = {
@@ -30,16 +30,21 @@ async function getEvents(): Promise<EventData[]> {
   return data.entries;
 }
 
-export const EventList: React.FC = async () => {
-  const events = await getEvents();
-  const currentDate = moment().tz("Australia/Sydney").startOf('day'); // Set the current date to start of the day in AEST
+export const EventList: React.FC = () => {
+  const [events, setEvents] = useState<EventData[]>([]);
 
+  useEffect(() => {
+    getEvents().then(setEvents);
+  }, []);
+
+  const currentDate = moment().tz("Australia/Sydney").startOf('day'); // Set the current date to start of the day in AEST
   const futureEvents = events.filter(({ event }) => moment(event.start_at).tz("Australia/Sydney") >= currentDate);
 
   return (
     <div className="grid gap-4">
       {futureEvents.map(({ event, api_id }) => (
         <a className="event-box" key={api_id} href={event.url} target="_blank" rel="noopener noreferrer">
+          <img src={event.cover_url} alt={event.name} className="event-image" />
           <p className="text-sm text-gray-500 dark:text-gray-400">
             {moment(event.start_at).tz("Australia/Sydney").format('MMMM Do YYYY')}
           </p>
