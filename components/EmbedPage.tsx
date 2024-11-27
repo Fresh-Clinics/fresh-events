18b8b4bedc8bc17e87366cc15117e2bf0838c0cc
Fresh-Clinics <community@freshclinics.com>
2024-11-27 18:33:50 +1100

    Create EmbedPage.tsx

---
+++ components/EmbedPage.tsx
@@ -0,0 +1,10 @@
+<iframe
+  src="https://lu.ma/embed/calendar/cal-MKTWCUNpEanNkY9/events?lt=light"
+  width="600"
+  height="450"
+  frameborder="0"
+  style="border: none!important; background:#ffffff!important; background-color:#ffffff!important"
+  allowfullscreen=""
+  aria-hidden="false"
+  tabindex="0"
+></iframe>
