b484bab69947ac55933468516537c5aa46f60cb0
Fresh-Clinics <community@freshclinics.com>
2024-11-27 18:34:18 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { HomePage } from "@/components/HomePage";
+import { HomePage } from "@/components/EmbedPage";
 
 export default function Home() {
-  return <HomePage />;
+  return <EmbedPage />;
 }
