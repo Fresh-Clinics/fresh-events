b7fae2dc5c6bf41715e0f6d937b11e279c2916fe
Fresh-Clinics <community@freshclinics.com>
2025-01-08 18:09:43 +1100

    Update HomePage.tsx

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,10 +1,19 @@
+import EventList from "@/components/EventList";
 
-    <iframe
-  src="https://lu.ma/embed/calendar/cal-PfKCHyYHQHvdr87/events"
-  frameborder="0"
-  style="border: none; border-radius: 0px"
-  allowfullscreen="true"
-  aria-hidden="false"
-  tabindex="0"
-></iframe>
-  
+export function HomePage() {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <main className="flex-1">
+        <section className="w-full py-12 md:py-24 lg:py-32">
+          <div className="container px-4 md:px-6">
+            <div className="grid gap-10">
+              <div className="space-y-4">
+                <EventList />
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
