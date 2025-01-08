cf3e158afe39c3a0cd0d6ee76b6f062664ce2041
Fresh-Clinics <community@freshclinics.com>
2025-01-08 18:09:11 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import { HomePage } from "@/components/HomePage";
-
-export default function Home() {
-  return <HomePage />;
-}
+<iframe
+  src="https://lu.ma/embed/calendar/cal-PfKCHyYHQHvdr87/events"
+  width="1000"
+  height="1000"
+  frameborder="0"
+  style="border: none; border-radius: 0px;"
+  allowfullscreen="true"
+  aria-hidden="false"
+  tabindex="0"
+></iframe>
