6092cec04c8bd31d537441a7762d2caca8e16e03
Fresh-Clinics <community@freshclinics.com>
2025-01-08 18:19:00 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,29 @@ import React from "react";
 
 const Page: React.FC = () => {
   return (
-    <div style={{ background:"#f4f5f6!important", display: "flex", justifyContent: "left", alignItems: "start", height: "100vh" }}>
+    <div
+      style={{
+        background: "#f4f5f6",
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        height: "100vh",
+      }}
+    >
       <iframe
         src="https://lu.ma/embed/calendar/cal-PfKCHyYHQHvdr87/events"
         width="1000"
         height="1000"
         frameBorder="0"
-        style={{ border: "none", borderRadius: "0px", background:"#f4f5f6!important" }}
-        allowFullScreen="true"
+        style={{
+          border: "none",
+          borderRadius: "0px",
+          background: "#f4f5f6",
+        }}
+        allowFullScreen
         aria-hidden="false"
         tabIndex={0}
-      />
+      ></iframe>
     </div>
   );
 };
