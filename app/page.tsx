7ea619a6affd9e358d61495f735343c079067a2e
Fresh-Clinics <community@freshclinics.com>
2025-01-08 18:15:46 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,15 @@ import React from "react";
 
 const Page: React.FC = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+    <div style={{ display: "flex", justifyContent: "left", alignItems: "left", height: "100vh", padding:"50px" }}>
       <iframe
         src="https://lu.ma/embed/calendar/cal-PfKCHyYHQHvdr87/events"
         width="1000"
         height="1000"
         frameBorder="0"
-        style={{ border: "none", borderRadius: "0px" }}
-        allowFullScreen
+
+        style={{ border: "none", borderRadius: "0px", background:"#f4f5f6!important" }}
+        allowFullScreen="true"
         aria-hidden="false"
         tabIndex={0}
       />
